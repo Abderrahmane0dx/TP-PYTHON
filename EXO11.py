b87a488c3cfce35a06ae6c24be742955e9f46300
Abderrahmane0dx <benamaraabderrahmane2@gmail.com>
2024-12-09 22:56:56 +0100

    modification-exercice11

--- EXO11.py
+++ EXO11.py
@@ -1,5 +1,9 @@
 width=int(input("Please Enter The Width Of Your Line:"))
+height=int(input("Please Enter The height Of Your Line:"))
 
 print("The Width That You Have Chousen Is:",width)
+print("The height That You Have Chousen Is:",height)
 #usage de la multiplication entre le caractère # et la taille:
-print("Here Is Your Line With Hash Characters:",'#'*width)
+print("Here Is Your Rectangle With Hash Characters:")
+for i in range(height):
+    print('#'*width)
