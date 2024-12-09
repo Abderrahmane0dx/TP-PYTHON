9f18764701da733304a154df85727a9144d15ad8
Abderrahmane0dx <benamaraabderrahmane2@gmail.com>
2024-12-09 22:51:58 +0100

    exercice11

---
+++ EXO11.py
@@ -0,0 +1,5 @@
+width=int(input("Please Enter The Width Of Your Line:"))
+
+print("The Width That You Have Chousen Is:",width)
+#usage de la multiplication entre le caractère # et la taille:
+print("Here Is Your Line With Hash Characters:",'#'*width)
